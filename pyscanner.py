ee0ee4596c0d37832932ea6d1889c6ea33235a76
Jonathan Dutson <jonathandutson21@gmail.com>
2018-09-29 16:57:10 -0600

    basic scanner working i think

--- pyscanner.py
+++ pyscanner.py
@@ -2,19 +2,11 @@
 # Base code here: https://gist.github.com/TheZ3ro/7255052
 
 from threading import Thread
+from multiprocessing.dummy import Pool as ThreadPool 
 import argparse
 import socket
 
-def parse_args():
-	parser = argparse.ArgumentParser(description='Process some integers.')
-	parser.add_argument('host', metavar='123.123.123.123', help='specifies the host to be scanned')
-	parser.add_argument('-p', metavar='1-1023', help='specifies the host to be scanned')
-	args = parser.parse_args()
-	print(args.host)
-
-# host = raw_input('host > ')
-# from_port = input('start scan from port > ')
-# to_port = input('finish scan to port > ')   
+host = ""
 counting_open = []
 counting_close = []
 threads = []
@@ -25,20 +17,29 @@ def scan(port):
 	print('working on port > '+(str(port)))      
 	if result == 0:
 		counting_open.append(port)
-		#print((str(port))+' -> open') 
 		s.close()
 	else:
 		counting_close.append(port)
-		#print((str(port))+' -> close') 
 		s.close()
 
-parse_args()
-# for i in range(from_port, to_port+1):
-# 	t = Thread(target=scan, args=(i,))
-# 	threads.append(t)
-# 	t.start()
+def main():
+	parser = argparse.ArgumentParser(description='Process some integers.')
+	parser.add_argument('host', metavar='123.123.123.123', help='specifies the host to be scanned')
+	parser.add_argument('-p', '--ports', metavar='1-1023', help='specifies the host ports to be scanned')
+	args = parser.parse_args()
 	
-# [x.join() for x in threads]
+	host = args.host
+	if args.ports:
+		ports = args.ports.split('-')
+		from_port = int(ports[0])
+		to_port = int(ports[-1])
+	else:
+		from_port = 1
+		to_port = 1
 
-# print(counting_open)
-	
+	pool = ThreadPool(100)
+	pool.map(scan, range(from_port, to_port+1))
+
+	print(counting_open)
+	print(counting_close)
+main()
