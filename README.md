c98db4088cae69a60b096d0efdadf37a973add8c
Jonathan Dutson <jonathandutson21@gmail.com>
2018-09-29 17:32:41 -0600

    Generating HTML report

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Python-Port-Scanner
+
+To install required packages, run the following command:
+    sudo pip install -r requirements.txt
--- pyscanner.py
+++ pyscanner.py
@@ -3,13 +3,25 @@
 
 from threading import Thread
 from multiprocessing.dummy import Pool as ThreadPool 
+from yattag import Doc
 import argparse
 import socket
 
 host = ""
+portstring = ""
 counting_open = []
 counting_close = []
 threads = []
+
+def generate_html():
+	doc, tag, text = Doc().tagtext()
+	with tag('hi'):
+		text('Port Scan Report')
+		with tag('h2'):
+			text("Host: " + host)
+		with tag('h2'):
+			text("Ports: " + portstring)
+	print(doc.getvalue())
 
 def scan(port):
 	s = socket.socket()
@@ -28,8 +40,11 @@ def main():
 	parser.add_argument('-p', '--ports', metavar='1-1023', help='specifies the host ports to be scanned')
 	args = parser.parse_args()
 	
+	global host
 	host = args.host
 	if args.ports:
+		global portstring
+		portstring = args.ports
 		ports = args.ports.split('-')
 		from_port = int(ports[0])
 		to_port = int(ports[-1])
@@ -39,7 +54,9 @@ def main():
 
 	pool = ThreadPool(100)
 	pool.map(scan, range(from_port, to_port+1))
+	counting_open.sort()
 
+	print("Open ports: ")
 	print(counting_open)
-	print(counting_close)
+	generate_html()
 main()
